f5b4eb8923885c8c1551872ed2157512cd408bc9
ZhaoXiongNi <1312529866@qq.com>
2023-09-20 06:24:32 +0800

    报告主体完工

--- 202121331017/code/work_0919/src/main/java/org/example/Content.java
+++ 202121331017/code/work_0919/src/main/java/org/example/Content.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
 public class Content {
+    //文件的读取
     public static void getContent(String filePath, StringBuffer content) {
         BufferedReader fileReader;
         if(!new File(filePath).exists()){
--- 202121331017/code/work_0919/src/test/java/PageTest.java
+++ 202121331017/code/work_0919/src/test/java/PageTest.java
@@ -6,8 +6,11 @@ public class PageTest {
     public void mainTest() throws Exception{
         //主测试函数，用以完成绝大多数测试
         String[] arg = new String[3];
+        //noinspection SpellCheckingInspection
         arg[0] = "D:\\Table\\各科文件\\工程概论\\ZhaoXiongNi\\202121331017\\testFile\\org.txt";
+        //noinspection SpellCheckingInspection
         arg[1] = "D:\\Table\\各科文件\\工程概论\\ZhaoXiongNi\\202121331017\\testFile\\org_add.txt";
+        //noinspection SpellCheckingInspection
         arg[2] = "D:\\Table\\各科文件\\工程概论\\ZhaoXiongNi\\202121331017\\testFile\\ans.txt";
 
         Main.main(arg);
@@ -17,8 +20,11 @@ public class PageTest {
     public void pathErrorTest() throws Exception{
         //测试路径错误
         String[] arg = new String[3];
+        //noinspection SpellCheckingInspection
         arg[0] = "D:\\Table\\各科文件\\工程概论\\ZhaoXiongNi\\202121331017\\errorPath\\org.txt";
+        //noinspection SpellCheckingInspection
         arg[1] = "D:\\Table\\各科文件\\工程概论\\ZhaoXiongNi\\202121331017\\testFile\\org_add.txt";
+        //noinspection SpellCheckingInspection
         arg[2] = "D:\\Table\\各科文件\\工程概论\\ZhaoXiongNi\\202121331017\\testFile\\ans.txt";
 
         Main.main(arg);
