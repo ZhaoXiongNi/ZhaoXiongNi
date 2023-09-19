b99e97e6d8a653e0f98d9c53aa1b81f3cfcbd4e4
ZhaoXiongNi <1312529866@qq.com>
2023-09-20 02:58:33 +0800

    异常处理

--- 202121331017/code/work_0919/src/main/java/org/example/Main.java
+++ 202121331017/code/work_0919/src/main/java/org/example/Main.java
@@ -11,6 +11,7 @@ import com.hankcs.hanlp.HanLP;
 import com.hankcs.hanlp.seg.common.Term;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.PrintWriter;
 import java.util.HashMap;
@@ -19,21 +20,22 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws Exception{
         //从命令行获取文件路径
-        String filePath1 = args[0];     //抄袭文件1路径
-        String filePath2 = args[1];     //抄袭文件2路径
+        String filePath1 = args[0];     //抄袭文件1的路径
+        String filePath2 = args[1];     //抄袭文件2的路径
         String outputPath = args[2];   //输出相似率路径
 
         //读取文件内容
-        BufferedReader fileReader1 = new BufferedReader(new FileReader(filePath1)); //文件1内容读取器
-        BufferedReader fileReader2 = new BufferedReader(new FileReader(filePath2)); //文件2内容读取器
-        String content1 = fileReader1.readLine();   //获取文件1内容
-        String content2 = fileReader2.readLine();   //获取文件2内容
-        fileReader1.close();    //关闭读取器
-        fileReader2.close();    //关闭读取器
+        //创建文章结果
+        StringBuffer content1 = new StringBuffer();
+        StringBuffer content2 = new StringBuffer();
+
+        //调用自定义函数读取文件中的文字
+        getContent(filePath1, content1);
+        getContent(filePath2, content2);
 
         //使用HanLP分词器进行分词,Term对象代表句子中的一个词
-        List<Term> fileWords1 = HanLP.segment(content1);    //词语集1，文件1的内容
-        List<Term> fileWords2 = HanLP.segment(content2);    //词语集2，文件2的内容
+        List<Term> fileWords1 = HanLP.segment(content1.toString().trim());    //词语集1，文件1的内容,去除空格
+        List<Term> fileWords2 = HanLP.segment(content2.toString().trim());    //词语集2，文件2的内容,去除空格
 
         //创建HashMap存储词频向量，我在使用二维数组作为value统计两个文件中的值
         HashMap<String, int[]> wordMap = new HashMap<>();
@@ -69,12 +71,37 @@ public class Main {
 
         // 将结果写入输出文件，计算出的值为范围在0~1之间的余弦相似度，需要*100才是相似率
         try (PrintWriter out = new PrintWriter(outputPath)) {
-            out.printf("%.2f %%", res*100);    //输出为浮点型的保留两位小数
+            out.printf("%.2f%%", res*100);    //输出为浮点型的保留两位小数
         }
 
         //终端输出测试答案,计算出的值为范围在0~1之间的余弦相似度，需要*100才是相似率
         System.out.printf("相似率：%.2f %%, 结果成功已转存到文件中\n",res*100);
     }
+
+    private static void getContent(String filePath, StringBuffer content) {
+        BufferedReader fileReader;
+        if(!new File(filePath).exists()){
+            System.out.println("文件路径"+filePath+"不存在");   //文件不存在报错
+        }
+        try {
+            fileReader = new BufferedReader(new FileReader(filePath)); //文件内容读取器
+            String firstLine;
+            // 首先检查文件是否为空
+            if ((firstLine = fileReader.readLine()) == null) {
+                System.out.println("文件路径"+filePath+"为空");
+                return;
+            }
+            content.append(firstLine);
+
+            while (null != fileReader.readLine()){
+                content.append(fileReader.readLine());   //获取文件内容
+            }
+            fileReader.close();    //关闭读取器
+        }catch (Exception e){
+            System.out.println("读取失败");
+        }
+    }
+
     // 计算点积
     private static double dotProduct(HashMap<String, int[]> wordMap) {
         double res = 0;
